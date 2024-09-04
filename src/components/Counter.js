7c84fc83fbd64c90ea3f1edd42f0a86b621d81bf
vijithhg <vijithhgedu@gmail.com>
2024-09-04 20:21:51 +0530

    count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 const Counter = ()=>{
+    const[count,setCount]=useState(0)
+
+    const incrementHandler = ()=>{
+        setCount(count+1)
+    }
+
+    const decrementHandler=()=>{
+        setCount(count-1)
+    }
+    const resetHandler = ()=>{
+        setCount(0)
+    }
+   
     return(
         <>
             <h2>Counter</h2>
+
+            <h2>Count : {count}</h2>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+            <button onClick={resetHandler}>Reset</button>
+
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button>
+            <button onClick={()=>setCount(0)}>Reset</button>
+
         </>
     )
 }
